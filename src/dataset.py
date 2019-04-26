087b9b99eca89e7ff6b8553056b376a855f0ad3a
Garry Kling <garry.kling@gmail.com>
2019-04-25 23:25:07 -0700

    Start of planning for multi-level CLI tools

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,44 @@
 """
 dataset.py
 
-Functions to perform certain operations on datasets
+Functions to perform certain operations on datasets.
+Automates the creation of some folders and files for a dataset.
 
-* c
+  ../datasets/<DATASET_NAME>
+        ├── <DATASET_NAME>.md (Info about a dataset)
+        ├── dataset_info.csv
+        ├── audio
+        │   ├── audio_info.csv
+        │   ├── audio_files...
+        │   └...
+        │
+        ├── score_data (this is a bit undefined as yet)
+        │   ├── score_info.csv
+        │   ├── notation_files...
+        │   ├── score_files...
+        │   └...
+        │
+        └── midi_files (transcriptions of audio data)
+            ├── midi_info.csv
+            ├── midi_files...
+            └...
 """
+
+class Dataset:
+    def __init__(self):
+
+
+
+def create_dataset(datasetName: str):
+    """
+    Factory function for Dataset.
+    Starts the process of creating a dataset
+    :param datasetName: the name of the dataset to create
+    :param instrumentName: the name of the instrument captured
+    :return: a Dataset object
+    """
+
+def main():
+
+if __name__ == '__main__':
+    main()

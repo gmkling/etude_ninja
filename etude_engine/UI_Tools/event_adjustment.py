5106dd18aac85021bb78928145e6e8a2853b749e
Garry Kling <garry.kling@gmail.com>
2019-10-19 13:45:56 -0700

    Doing some experimentation with widgets and events in the browser

--- etude_engine/UI_Tools/event_adjustment.py
+++ etude_engine/UI_Tools/event_adjustment.py
@@ -6,9 +6,14 @@ in a sound file.
 
 '''
 
+import argparse
+from pathlib import Path
+
 import numpy as np
 from scipy.io import wavfile
 from bokeh.io import show
+from bokeh.events import ButtonClick
+from bokeh.models import Button
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, RangeTool
 from bokeh.plotting import figure
@@ -21,15 +26,18 @@ def load_wav(filename):
     :return: ColumnDataSource, sRate (sampleRate)
     '''
     sRate, data = wavfile.read(filename)
-    source = ColumnDataSource({'y': data, 'x': np.arange(0, len(data), 1)})
-    return source, sRate
+    audioSource = ColumnDataSource({'y': data, 'x': np.arange(0, len(data), 1)})
+    return audioSource, sRate
 
-def plot_wav_range():
+def play_selection(event):
+    print("Play action")
+
+def plot_wav_range(audioSource):
     p = figure(plot_height=300, plot_width=800, tools="xpan", toolbar_location=None,
                x_axis_type="linear", x_axis_location="above",
                background_fill_color="#efefef", x_range=(2000, 12500))
 
-    p.line('x', 'y', source=source)
+    p.line('x', 'y', source=audioSource)
     p.yaxis.axis_label = 'Audio'
 
     select = figure(title="Drag the middle and edges of the selection box to change the range above",
@@ -40,13 +48,15 @@ def plot_wav_range():
     range_tool = RangeTool(x_range=p.x_range)
     range_tool.overlay.fill_color = "navy"
     range_tool.overlay.fill_alpha = 0.2
+    play_button = Button(label="Play Selection")
+    play_button.on_event(ButtonClick, play_selection)
 
-    select.line('x', 'y', source=source)
+    select.line('x', 'y', source=audioSource)
     select.ygrid.grid_line_color = None
     select.add_tools(range_tool)
     select.toolbar.active_multi = range_tool
 
-    show(column(p, select))
+    show(column(p, select, play_button))
 
 def main():
     ''''
@@ -54,5 +64,17 @@ def main():
         User will use web UI to adjust the in and out points visually, with the assistance
         of audio playback of segments. 
     '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--fname', metavar='Filename', type=str, nargs=1,
+                        help='Name of the audio file to input.')
+    args = parser.parse_args()
+    filepath = args.fname
+
+    if filepath:
+        #filename = str(args.fname)
+        print(Path(str(filepath)))
+        audio, fs = load_wav(filepath[0])
+        plot_wav_range(audio)
+
 if __name__ == '__main__':
     main()
